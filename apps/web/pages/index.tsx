606ba6be5e8a98edbc47cc93e1198cbd89906821
Preeti Singh <singhp1992@gmail.com>
2024-02-07 09:24:08 +0100

    initial commit

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'ui'
+
+export default function Web() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>Web</h1>
+      <Button />
+    </div>
+  )
+}
---
+++ packages/ui/Button.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export const Button = () => {
+    return <button className="bg-green-400">Button</button>;
+};
