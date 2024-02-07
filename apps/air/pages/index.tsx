9bab6bb51af892de838472c374b6b378a57597c8
Preeti Singh <singhp1992@gmail.com>
2024-02-07 10:57:27 +0100

    fix: global styles and air app project setup

--- apps/air/pages/index.tsx
+++ apps/air/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Air() {
   return (
     <div>
-      <h1>Air</h1>
+      <h1 className="text-2xl font-bold">Air</h1>
       <Button />
     </div>
   );
