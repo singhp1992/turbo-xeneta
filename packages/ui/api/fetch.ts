7bab47b07f1917619dd2914bf2934c4ffd7094c8
Preeti Singh <singhp1992@gmail.com>
2024-02-10 12:36:16 +0100

    fix: using T in fetch data

--- packages/ui/api/fetch.ts
+++ packages/ui/api/fetch.ts
@@ -1,22 +1,23 @@
-interface FetchDataProps {
+import { MarketRate } from "../utils/types";
+
+interface FetchDataProps<T> {
   url: string;
-  setState: (data: any[]) => void;
+  setState: (data: T[]) => void;
   setLoading: (loading: boolean) => void;
   setError: (error: boolean) => void;
 }
 
-export const fetchData = async ({
-  url,
-  setState,
-  setLoading,
-  setError,
-}: FetchDataProps) => {
+export const fetchData = async <T>(props: FetchDataProps<T>) => {
+  const { url, setState, setLoading, setError } = props;
   try {
     const res = await fetch(url, {
       headers: {
         "x-api-key": `${process.env.NEXT_PUBLIC_API_KEY}`,
       },
     });
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.statusText}`);
+    }
     const data = await res.json();
     setState(data);
     setLoading(false);
